e6b5e260f1b64075a1bc524f92f1e00658896925
Diamond983 <k.zagdanski1@op.pl>
2018-05-01 13:57:49 +0200

    fiew new name changes

--- Unit.js
+++ Unit.js
@@ -9,17 +9,18 @@ class Unit
         this.x = x;
         this.y = y;
         this.r = r;
-        this.angle = 0;
-        this.fitnes = 0; // how good unit is
-        this.speed = 5;
-        this.time = 0; // how long was alive 
+        this.rotation = 0;
+        this.fitness = 0; // how good unit is
+		this.speed = 5;
+		this.turnSpeed = 10;
+        this.ttc = 0; // time to complete
         this.alive = true;
         this.win = false; // complete task (now)
         this.lastWinner = false; // complete task in last gen and was put in new gen
         this.best = false; // -||- was close 
         this.distance = 999999; // distance from target
 		this.movePos = 0; // index of move in DNA
-		this.colider = "give here colider";
+		this.colider = new Colider(this.x, this.y, this.r);
         if(dna)
         {
             this.dna = dna;
@@ -43,18 +44,18 @@ class Unit
 		{
 			// DNA[index] == -1(left) 0(forward) 1(right)
 			if (this.dna[this.movePos] == -1)
-				this.angle -= 10; //1.6*this.speed;
+				this.rotation -= this.turnSpeed;
 			if (this.dna[this.movePos] == 1)
-				this.angle += 10;//1.6*this.speed;
+				this.rotation += this.turnSpeed;
 
-			if(this.angle > 360)
-				this.angle -= 360;
-			if(this.angle < 0)
-				this.angle += 360;
-			this.x += this.speed * Math.cos(this.angle * Math.PI / 180);
-    		this.y += this.speed * Math.sin(this.angle * Math.PI / 180);
+			if(this.rotation > 360)
+				this.rotation -= 360;
+			if(this.rotation < 0)
+				this.rotation += 360;
+			this.x += this.speed * Math.cos(this.rotation * Math.PI / 180);
+    		this.y += this.speed * Math.sin(this.rotation * Math.PI / 180);
 			
-			this.time++;
+			this.ttc++;
     		this.movePos++;
 		}
     }
@@ -103,8 +104,8 @@ class Unit
 
 		ctx.beginPath();
 		ctx.moveTo(this.x,this.y);
-		let a = this.x + this.r * Math.cos(this.angle * Math.PI / 180);
-    	let b = this.y + this.r * Math.sin(this.angle * Math.PI / 180);
+		let a = this.x + this.r * Math.cos(this.rotation * Math.PI / 180);
+    	let b = this.y + this.r * Math.sin(this.rotation * Math.PI / 180);
 		ctx.lineTo(a,b);
 		ctx.stroke();
     }
